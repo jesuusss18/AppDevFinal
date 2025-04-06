c9c18fd513c9ee3f79591c10e8029cadc216db26
jesuusss18 <jesus.jesuscalvojr@gmail.com>
2025-04-06 12:33:02 -0500

    Update urls.py

--- my_app/urls.py
+++ my_app/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('expenses', views.ExpensesListView.as_view()),
+    path('expenses', views.ExpensesListView.as_view(), name='expenses'),
     path('accounts/register/', views.register, name='register'),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
