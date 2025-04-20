8d458f8020a4d9cc6f692aa40b35a09a9c120824
jesuusss18 <jesus.jesuscalvojr@gmail.com>
2025-04-19 22:06:49 -0500

    Refactors expense tracker to manage liabilities
    Updates the expense tracker to handle liabilities with long-term options,
    including interest rates and end dates.
    
    - Modifies the ExpenseForm to LiabilityForm.
    - Updates the Expense model to Liability.
    - Implements monthly expense calculation for long-term liabilities.
    - Integrates Plotly graph to visualize monthly expenses.

--- my_app/models.py
+++ my_app/models.py
@@ -1,38 +1,45 @@
 from django.db import models
+from django.db.models import Sum, Count, F, Q
 from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
-# Create your models here.
 
-class  Account(models.Model):
+class Account(models.Model):
     name = models.CharField(max_length=100)
+    balance = models.FloatField(default=0)
+    income = models.FloatField(default=0)
     expense = models.FloatField(default=0)
     user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    expenses_list =  models.ManyToManyField('Expense',blank=True)
+    saving_goal = models.FloatField(default=0)
+    liability_list = models.ManyToManyField('Liability', blank=True)
+    salary = models.FloatField(default=0)
 
-class Expense(models.Model):
-    name =  models.CharField(max_length=100)
+
+class Liability(models.Model):
+    name = models.CharField(max_length=100)
     amount = models.FloatField(default=0)
-    date = models.DateField(null=False, default= datetime.now().date)
+    date = models.DateField(null=False, default=datetime.now().date())
     long_term = models.BooleanField(default=False)
-    interest_rate = models.FloatField(default=0,null=True,blank=True)
-    end_date = models.DateField(null=True,blank=True)
-    monthly_expenses = models.FloatField(default=0,null=True,blank=True)
+    interest_rate = models.FloatField(default=0, blank=True, null=True)
+    end_date = models.DateField(blank=True, null=True)
+    monthly_expense = models.FloatField(default=0, blank=True, null=True)
     user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
 
-
-    def save(self, *arg, **kwargs):
+    
+    def save(self, *args, **kwargs):
         if self.long_term:
-            self.monthly_expenses = self.calculate_monthly_expense()
-        super(Expense ,self).save(*arg, **kwargs)
+            self.monthly_expense = self.calculate_monthly_expense()
+        super(Liability, self).save(*args, **kwargs)
 
 
-    def calculate_monthly_expenses(self):
+    def calculate_monthly_expense(self):
         if self.long_term:
-            if self.interest_rate:
-                return self.amount / ((self.end_date -self.date) /30 )
+            if self.interest_rate == 0:
+                return self.amount / ((self.end_date - self.date) / 30)  # Assuming a month has 30 days
             else:
-                months = (self.end_date.year-datetime.now().year * 12 + self.end_date.month - datetime.now().month)
-                monthly_rate = self.interest / 12 / 100
-                monthly_expense = (self.amount * monthly_rate) / (1-(1 + monthly_rate) ** -months)
+                months = (self.end_date.year - datetime.now().year) * 12 + self.end_date.month - datetime.now().month
+                monthly_rate = self.interest_rate / 12 / 100
+                monthly_expense = (self.amount * monthly_rate) / (1 - (1 + monthly_rate) ** -months)
+                return round(monthly_expense, 2)
         else:
-            return self.monthly_expenses
+            return self.monthly_expense
--- my_app/urls.py
+++ my_app/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path,include
+#from django.conf.urls import url
+from django.urls import path, include
 from . import views
-
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('expenses', views.ExpensesListView.as_view()),
+    path('expenses', name='expenses', view=views.ExpenseListView.as_view()),
     path('accounts/register/', views.register, name='register'),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
