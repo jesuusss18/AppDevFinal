536ed9e8a7ddcfe777f5b6733d76e8d49dce69b3
jesuusss18 <jesus.jesuscalvojr@gmail.com>
2025-04-27 22:16:40 -0500

    Comments

--- my_app/models.py
+++ my_app/models.py
@@ -1,7 +1,4 @@
 from django.db import models
-from django.db.models import Sum, Count, F, Q
-from datetime import datetime
-from dateutil.relativedelta import relativedelta
 from django.utils import timezone
 
 class Account(models.Model):
@@ -14,7 +11,6 @@ class Account(models.Model):
     liability_list = models.ManyToManyField('Liability', blank=True)
     salary = models.FloatField(default=0)
 
-
 class Liability(models.Model):
     name = models.CharField(max_length=100)
     amount = models.FloatField(default=0)
@@ -26,6 +22,7 @@ class Liability(models.Model):
     user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
 
     def save(self, *args, **kwargs):
+        # Calculate monthly expense for long-term liabilities
         if self.long_term:
             self.monthly_expense = self.calculate_monthly_expense()
         super(Liability, self).save(*args, **kwargs)
@@ -33,16 +30,12 @@ class Liability(models.Model):
     def calculate_monthly_expense(self):
         if self.long_term:
             if not self.end_date:
-                raise ValueError("End date must be provided for long-term liabilities.")
+                raise ValueError("End date must be provided.")
             if self.interest_rate == 0:
-                # Assuming 30 days in a month for calculation if interest rate is 0
-                return self.amount / ((self.end_date - self.date).days / 30)  
-            else:
-                months = (self.end_date.year - self.date.year) * 12 + self.end_date.month - self.date.month
-                if months <= 0:
-                    raise ValueError("End date must be later than the start date.")
-                monthly_rate = self.interest_rate / 12 / 100
-                monthly_expense = (self.amount * monthly_rate) / (1 - (1 + monthly_rate) ** -months)
-                return round(monthly_expense, 2)
-        else:
-            return self.monthly_expense
+                return self.amount / ((self.end_date - self.date).days / 30)
+            months = (self.end_date.year - self.date.year) * 12 + self.end_date.month - self.date.month
+            if months <= 0:
+                raise ValueError("End date must be later than start date.")
+            monthly_rate = self.interest_rate / 12 / 100
+            return round((self.amount * monthly_rate) / (1 - (1 + monthly_rate) ** -months), 2)
+        return self.monthly_expense
